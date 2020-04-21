d1860e2d95468a46667265d816aea6263bf7438e
Alex Xavier <ajxv20@gmail.com>
2020-04-21 12:32:57 -0300

    Melhorias no desafio do módulo 3 segundo CR

--- codenation/nodejs-0/src/index.js
+++ codenation/nodejs-0/src/index.js
@@ -1,25 +1,15 @@
 'use strict'
 
-let sequenciaFibonacci = [0, 1]
-
 const fibonacci = () => {
+	let sequenciaFibonacci = [0, 1]
+
 	for (let i = 2; i < 15; i++) {
 		sequenciaFibonacci[i] = sequenciaFibonacci[i-1] + sequenciaFibonacci[i-2]
 	}
 	return sequenciaFibonacci
 }
 
-const isFibonnaci = (numero) => {
-	const auxiliar = fibonacci()
-
-	for (let i = 0; i < auxiliar.length; i++) {
-		if (numero == auxiliar[i]) {
-			return true
-		}
-	}
-
-	return false
-}
+const isFibonnaci = (numero) => fibonacci().includes(numero)
 
 module.exports = {
   fibonacci,
