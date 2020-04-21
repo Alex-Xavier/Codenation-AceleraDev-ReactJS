858741f423cb3a68692dc37336713525c4987ce8
Alex Xavier <ajxv20@gmail.com>
2020-04-21 10:51:07 -0300

    fibonacci ok | isFibonacci em andamento

--- codenation/nodejs-0/src/index.js
+++ codenation/nodejs-0/src/index.js
@@ -1,10 +1,30 @@
 'use strict'
 
-const fibonacci = () => null
+let sequenciaFibonacci = [0, 1]
 
-const isFibonnaci = (num) => null
+const fibonacci = () => {
+	for (let i = 2; i < 15; i++) {
+		sequenciaFibonacci[i] = sequenciaFibonacci[i-1] + sequenciaFibonacci[i-2]
+	}
+	return sequenciaFibonacci
+}
+
+const isFibonnaci = (numero) => {
+	const auxiliar = fibonacci()
+	console.log(auxiliar)
+
+	auxiliar.map( (valor) => {
+		console.log(numero, valor)
+		if (numero == valor) {
+			console.log('ENTROU NO IF')
+			return true
+		}
+	});
+
+	return false
+}
 
 module.exports = {
-    fibonacci,
-    isFibonnaci
+  fibonacci,
+  isFibonnaci
 }
